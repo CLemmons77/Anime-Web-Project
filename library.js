9db39208fb9eadb8e05f2e6f3abed0b0e3012762
CLemmons77 <caleblego7@gmail.com>
2024-06-25 21:48:55 -0400

    second commit

--- library.js
+++ library.js
@@ -1,36 +1,49 @@
 // API: https://api.jikan.moe/v4/anime
 
-let results
-
-const animeList = document.querySelector("#results");
-
-async function onSearchChange(event) {
-    const results = event.target.value
-    renderAnime(results);
-}
+const animeList = document.querySelector(".anime__cards");
 
 async function renderAnime(results) {
-    const animes = await fetch("https://api.jikan.moe/v4/anime");
-    const animesInfo = await animes.json()
+    const response = await fetch("https://api.jikan.moe/v4/anime?q=${results}");
+    const animesInfo = await response.json();
+    
+    if (results === "ALPHABET_ASCEND") {
+        return anime.sort((a, b) => a.title - b.title)
+    }    
+    else if (results === "ALPHABET_DESCEND") {
+        return anime.sort((a, b) => b.title - a.title)
+    }    
+    else if (results === "RATING") {
+        return anime.sort((a, b) => a.rating - b.rating)
+    }    
 
-  
-}
-
-function animeHTML(anime) {
-    return `<div class="anime__card">
-    <div class="anime__card--container">
-      <figure class="anime__poster--wrapper">
-        <img src="${data.images.jpg.image_url}" alt="" class="anime__poster" />
-      </figure>
-      <h3 class="anime__title">${data.title}</h3>
-      <p><b>Episodes:</b> ${data.episodes}</p>
-      <p><b>Rating:</b> ${data.rating}</p>
-      <p><b>Score:</b> ${data.score}</p>
-      <p>
-        <b>Synopsis:</b> ${data.synopsis}
-      </p>
-    </div>
-  </div>`
+  animeList.innerHTML = animesInfo.data
+    .map((anime) => animeHTML(anime))
+    .join("");
 }
 
 renderAnime(results);
+
+function onSearchChange(event) {
+    return results = event.target.value
+}
+
+function filterAnime(event) {
+    renderAnime(event.target.value)
+}
+
+function animeHTML(anime) {
+  return `<div class="anime__card">
+        <div class="anime__card--container">
+            <figure class="anime__poster--wrapper">
+                <img src="${anime.image_url}" alt="" class="anime__poster" />
+            </figure>
+            <h3 class="anime__title">${anime.title}</h3>
+                <p><b>Episodes:</b> ${anime.episodes}</p>
+                <p><b>Rating:</b> ${anime.rating}</p>
+                <p><b>Score:</b> ${anime.score}</p>
+                <p>
+                    <b>Synopsis:</b> ${anime.synopsis}
+                </p>
+        </div>
+    </div>`;
+}
