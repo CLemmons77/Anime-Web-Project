5298ded2eb91b73e0f4c7d9d1fead9c2653fd0dd
CLemmons77 <caleblego7@gmail.com>
2024-07-12 13:11:42 -0400

    problem solving search results

--- library.js
+++ library.js
@@ -6,34 +6,61 @@ const resultLoading = document.querySelector(".result")
 async function renderAnime(results) {
     resultLoading.classList += ' results__loading'
     
-    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${results}`);
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${results || "no search results"}`);
     const animesInfo = await response.json();
     
     resultLoading.classList.remove('results__loading')
 
     animeList.innerHTML = animesInfo.data
+    .slice(0, 9)
     .map((anime) => animeHTML(anime))
     .join("");
-    
-    
-    
-    
+
 }
+
+async function intialAnime() {
+    resultLoading.classList += ' results__loading'
+    
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${`One Piece`}`);
+    const animesInfo = await response.json();
+    
+    resultLoading.classList.remove('results__loading')
+
+    animeList.innerHTML = animesInfo.data
+    .slice(0, 9)
+    .map((anime) => animeHTML(anime))
+    .join("");
+}
+
+intialAnime();
 
 async function filterAnime(event) {
-    if (event === "ALPHABET_ASCEND") {
-        return results.sort((a, b) => a.anime.title - b.anime.title)
-    }    
-    else if (event === "ALPHABET_DESCEND") {
-        return anime.sort((a, b) => b.anime.title - a.anime.title)
-    }    
-    else if (event === "RATING") {
-        return anime.sort((a, b) => a.anime.score - b.anime.score)
-    }    
-    console.log(event)
+    const filterValue = event.target.value;
+    const animeCards = [...document.querySelector(".anime__cards")];
+
+    animeCards.sort((a,b) => {
+        const titleA = a.querySelector(".anime__title").textContent;
+        const titleB = b.querySelector(".anime__title").textContent;
+        const scoreA = parseFloat(a.querySelector(".anime__score").textContent);
+        const scoreB = parseFloat(b.querySelector(".anime__score").textContent);
+
+        if (filterValue === "ALPHABET_ASCEND") {
+            return titleA.localeCompare(titleB);
+        }
+        else if (filterValue === "ALPHABET_DESCEND") {
+            return titleB.localeCompare(titleA);
+        }
+        else if (filterValue === "RATING") {
+            return scoreB - scoreA;
+        }
+    });
+
+    animeList.innerHTML = "";
+    animeCards.forEach((card) => animeList.appendChild(card));
 }
 
-function onSearchButtonClick() {
+function onSearchButtonClick(event) {
+    event.preventDefault();
     const query = document.querySelector('#search').value;
     renderAnime(query)
 }
