2607415cba85c63f3a547f3e0e03a3fc482fd7c9
CLemmons77 <caleblego7@gmail.com>
2024-07-24 14:55:12 -0400

    dark mode

--- library.js
+++ library.js
@@ -86,11 +86,29 @@ let isModalOpen = false
 let contrastToggle = false
 const scaleFactor = 1 / 20
  
- function toggleModal() {
+function toggleModal() {
     if (isModalOpen) {
        isModalOpen = false
        return document.body.classList.remove('modal--open')
     }
     isModalOpen = true
     document.body.classList += ' modal--open'
+}
+
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+       document.body.classList += ' dark-theme'
+    }
+    else {
+       document.body.classList.remove('dark-theme')
+    }
  }
